be7f6f0d3de04f7288c4340f7ff426fc7afbb909
Bojan SKI <codeucenje@gmail.com>
2025-01-09 10:16:17 +0100

    task done

--- check_name.php
+++ check_name.php
@@ -1,5 +1,13 @@
 <?php
-$userInputtedName = $_POST['userInputtedName'];
+$usersList = ['Toma', 'Petar', 'Marko'];
 
-var_dump($userInputtedName);
-?>
+(string) $userInputtedName = !empty(trim($_POST['userInputtedName'])) ? strtolower(trim($_POST['userInputtedName'])) : '';
+(bool) $isCorrectLength = strlen($userInputtedName) >= 3;
+
+if ((bool) !$isCorrectLength) die('The name needs to have 3 or more characters!');
+
+if (in_array(ucfirst($userInputtedName), $usersList)) {
+    echo "Match found";
+} else {
+    echo "Match not found";
+};
